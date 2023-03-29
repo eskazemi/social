cba6ff48a41ecd02509837f54e2b45e94760a975
Esmaeil kazemi <smk182018@gmail.com>
2023-03-29 10:36:10 +0430

    Feat(): create post model

--- social/home/models.py
+++ social/home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+    body = models.TextField()
+    slug = models.SlugField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}-{self.id}-{self.slug}"
+
