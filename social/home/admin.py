ff0fd6db3176312e5416625cd91367073e413906
Esmaeil kazemi <smk182018@gmail.com>
2023-03-29 10:36:40 +0430

    Feat(): customizing admin

--- social/home/admin.py
+++ social/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('body', 'slug', 'created_at', 'updated_at', 'user')
+    search_fields = ("slug",)
+    list_filter = ('created_at', 'updated_at')
+    prepopulated_fields = {"slug": ('body',)}
+    raw_id_fields = ("user", )
