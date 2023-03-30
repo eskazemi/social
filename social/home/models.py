a255766d9997f97b99580ca3c410746db03c73e4
eskazemi <smk182018@gmail.com>
2023-03-30 19:45:51 +0330

    Feat(): use get_absoluter_url function

--- social/home/models.py
+++ social/home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -11,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.__class__.__name__}-{self.slug}"
+
+    def get_absoluter_url(self):
+        return reverse('home:detail', args=(self.id, self.slug))
